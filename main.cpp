93f5e63516bfa619b0afe7683a21afdaa06e378c
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-04-11 23:41:10 -0400

    Quicksort

--- main.cpp
+++ main.cpp
@@ -701,7 +701,7 @@ int main() {
 
 //--------------- Binary Search Trees Main() ----------------------
 
-
+/*
 #include "BinSearchTree.h"
 
 
@@ -746,7 +746,7 @@ int main() {
 
 
 }
-
+*/
 
 //--------------- End - Binary Search Trees Main() ----------------------
 
@@ -837,3 +837,70 @@ int main(){
 
 
 //--------------- End Heap/Heapsort Main() ----------------------
+
+
+//--------------- Quicksort Main() ----------------------
+
+
+int partition (int x[], int lb, int ub){
+    int down, up, a, temp;
+
+    a = x[lb];  // the pivot element
+    up = ub;
+    down = lb;
+    //traverse the array and swap elements
+    while (down < up){
+        while (x[down] <= a && down < ub)
+            down++;
+        while (x[up] > a)
+            up--;
+        if (down < up){
+            temp = x[down];
+            x[down] = x[up];
+            x[up] = temp;
+        }
+    }
+
+    x[lb] = x[up];
+    x[up] = a;
+
+    //return the index where the pivot is now at
+
+    return up;
+} // end partition
+
+// main qsort method with array, lower and upper bound
+void quicksort(int x[], int lb, int ub){
+    // lower bound crossed upper bound
+    if(lb >= ub)	return;
+
+    // index j = the index of the "pivot"
+    int j = partition(x, lb, ub);
+    // ^^ this function will choose
+    // which element to partition
+    // around
+
+    quicksort(x, lb, j-1); // quicksort on left of j
+    quicksort(x, j+1, ub); // quicksort on the right of j
+}
+
+
+int main() {
+
+    int arr[] = {44, 66, 10, 1, 199, 100, 2, 88};
+
+    std::cout << "Before quicksort: ";
+    for (int x : arr) {
+        std::cout << x << " ";
+    }
+
+    quicksort(arr, 0, 7);
+    std::cout << "\nAfter quicksort: ";
+    for (int x : arr) {
+        std::cout << x << " ";
+    }
+
+
+}
+
+//--------------- End Quicksort Main() ----------------------
